fc12af235c0242a3eb336171f3d493614ee864d8
Ubuntu <mtulow@azvm.c4dgkxedsyhuxneth2rbzivjdh.bx.internal.cloudapp.net>
2022-12-18 13:35:03 +0000

    Added complete hello world example.

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
